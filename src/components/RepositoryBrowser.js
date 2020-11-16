71e110accf46f5894bb0cc3133daca619b66412f
Puria Golgoonnejad <puriagit@gmail.com>
2020-11-16 11:52:03 +1100

    fix select error

--- src/components/RepositoryBrowser.js
+++ src/components/RepositoryBrowser.js
@@ -98,8 +98,8 @@ class RepositoryBrowser extends Component {
             <div className="search-filter-area card">
                 <div>
                     <label>Repository type: </label>
-                    <select onChange={e => this._setFilter('repoType',e.target.value)}>
-                        <option selected value="all">All</option>
+                    <select defaultValue="all" onChange={e => this._setFilter('repoType',e.target.value)}>
+                        <option value="all">All</option>
                         <option value="sources">Not forked</option>
                         <option value="forks">Forked</option>
                     </select>
@@ -107,13 +107,13 @@ class RepositoryBrowser extends Component {
                 <div>
                     <label>Sort by: </label>
                    <div className="form-group">
-                        <select onChange={e => this._setFilter('repoSort',e.target.value)}>
-                            <option selected value="full_name">Full name</option>
+                        <select defaultValue="full_name" onChange={e => this._setFilter('repoSort',e.target.value)}>
+                            <option value="full_name">Full name</option>
                             <option value="created">Created time</option>
                             <option value="updated">Updated time</option>
                         </select>
-                        <select onChange={e => this._setFilter('repoDirection',e.target.value)}>
-                            <option selected value="asc">Ascending</option>
+                        <select defaultValue="asc" onChange={e => this._setFilter('repoDirection',e.target.value)}>
+                            <option value="asc">Ascending</option>
                             <option value="desc">Descending</option>
                         </select>
                    </div>
