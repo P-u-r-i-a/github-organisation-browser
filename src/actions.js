b791790c59bbc792c4f1db8df02e0114cf973a46
Puria Golgoonnejad <puriagit@gmail.com>
2020-11-20 08:23:19 +1100

    refactor the codes to use redux architecture
    * refactor the codes to use redux
    
    * small fixes
    
    * add .env - environment variables

--- src/actions.js
+++ src/actions.js
@@ -1,4 +1,12 @@
-import { SET_CONTRIBUTORS, CLOSE_MODAL } from "./values";
+import store from './store';
+import { SET_CONTRIBUTORS, 
+         CLOSE_MODAL, 
+         SET_ORGANISATION, 
+         SET_REPOSITORIES, 
+         ADD_MORE_REPOSITORIES, 
+         SET_VALUE,
+         NO_MORE_RESULT } from "./values";
+        
 
 // fetch the repository's contributors
  export let fetchContributors = (org, repo) => (dispatch) => {
@@ -20,5 +28,72 @@ import { SET_CONTRIBUTORS, CLOSE_MODAL } from "./values";
 export let closeModal = () => {
     return {
         type: CLOSE_MODAL
+    }
+}
+
+// fetch organisation details
+export let fetchOrganisationDetails = () => (dispatch) => {
+    let { orgName } = store.getState().main;
+
+    fetch(`https://api.github.com/orgs/${orgName}`)
+    .then((res) => {
+        return res.json();
+        }).then(orgDetails => {
+            dispatch({
+                type: SET_ORGANISATION,
+                payload: orgDetails
+            });
+        })
+    .catch(error => {
+        console.error(error);
+    });
+}
+
+// fetch organisation's repositories
+export let fetchRepositories = (initialRequest = false, page = store.getState().main.currentPage) => (dispatch) => {
+    let { orgName, perPage, repoType, repoSort, repoDirection } = store.getState().main;
+    
+    fetch(`https://api.github.com/orgs/${orgName}/repos?` + new URLSearchParams({
+        per_page: perPage,
+        page: page,
+        type: repoType,
+        sort: repoSort,
+        direction: repoDirection, 
+    }))
+    .then((res) => {
+        return res.json();
+        }).then(repos => {
+            if(initialRequest){
+                dispatch({
+                    type: SET_REPOSITORIES,
+                    payload: repos
+                });
+            } else {
+                if(repos.length > 0){
+                    dispatch({
+                        type: ADD_MORE_REPOSITORIES,
+                        payload: repos
+                    });
+                } else {
+                    dispatch({
+                        type: NO_MORE_RESULT
+                    });
+                }
+            }
+        })
+    .catch(error => {
+        console.error(error);
+    });
+    
+}
+
+// apply search filter
+export let setValue = (name, value) => {
+    return {
+        type: SET_VALUE,
+        payload: {
+            name: name,
+            value: value
+        }
     }
 }
--- src/values.js
+++ src/values.js
@@ -1,2 +1,7 @@
 export const SET_CONTRIBUTORS = 'set_contributors';
 export const CLOSE_MODAL = 'close_modal';
+export const SET_ORGANISATION = 'set_organisation';
+export const SET_REPOSITORIES = 'set_repositories';
+export const ADD_MORE_REPOSITORIES = 'add_more_repositories';
+export const SET_VALUE = 'set_value';
+export const NO_MORE_RESULT = 'no_more_result';
